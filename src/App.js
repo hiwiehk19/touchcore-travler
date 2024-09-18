b5810b9017aace9a49ddb04600dac4d60f2dfc8e
hiwiehk19 <hiwotzelalem3@gmail.com>
2024-09-18 17:00:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+// import HeroSection from "./components/HeroSection";
+// import FastestWay from "./components/FastestWay";
+// import EverythingYouNeed from "./components/EverythingYouNeed";
+// import PaymentsAndOrder from "./components/PaymentsAndOrder";
+// import CallToAction from "./components/CallToAction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500">
+      <Header />
+      {/* <HeroSection />
+      <FastestWay />
+      <EverythingYouNeed />
+      <PaymentsAndOrder />
+      <CallToAction /> */}
     </div>
   );
 }
